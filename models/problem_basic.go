daba0bf4c05fc5c7c207219ec5b4d561984ab46e
好饭不怕晚 <lepengxi@163.com>
2022-10-29 22:03:05 +0800

    发送验证码

---
+++ models/problem_basic.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+// 1. gorm 字段忘记;结束
+// 2. 数据库里命名和 models 里命名不一致
+// Error 1054: Unknown column 'problem.category_id' in 'field list'
+
+type ProblemBasic struct {
+	gorm.Model
+
+	Identity string `gorm:"column:identity;type:varchar(36);" json:"identity"` // 问题表的唯一标识
+
+	ProblemCategories []*ProblemCategory `gorm:"foreignKey:problem_id;references:id" json:"problem_categories"` // 关联问题分类表
+	// CategoryId string `gorm:"column:category_id;type:varchar(255);" json:"category_id"` // 分类id,已逗号分隔
+	Title      string `gorm:"column:title;type:char(255);" json:"title"`           // 文章标题
+	Content    string `gorm:"column:content;type:text;" json:"content"`            // 文章正文
+	MaxRuntime int    `gorm:"column:max_runtime;type:int(11);" json:"max_runtime"` // 最大运行时间
+	MaxMem     int    `gorm:column"max_mem;type:int(11);" json:"max_mem"`          // 最大运行内存
+}
+
+func (table *ProblemBasic) TableName() string {
+	return "problem_basic"
+}
+
+func GetProblemList(keyword, categoryIdentity string) *gorm.DB {
+	// data := make([]*Problem, 0)
+	// DB.Find(&data)
+
+	// for _, v := range data {
+	// 	fmt.Printf("Problem ==> %v \n", v)
+	// }
+	tx := DB.Model(new(ProblemBasic)).Preload("ProblemCategories").Preload("ProblemCategories.CategoryBasic").
+		Where("title like ? OR content like ?", "%"+keyword+"%", "%"+keyword+"%")
+	if categoryIdentity != "" {
+		tx.Joins("RIGHT JOIN problem_category pc on pc.problem_id = problem_basic.id").
+			Where("pc.category_id = (SELECT cb.id FROM category_basic cb WHERE cb.identity = ? )", categoryIdentity) // 错误：cb ——> cd
+	}
+	// if categoryIdentity != "" {
+	// 	tx.Joins("RIGHT JOIN problem_category pc on pc.problem_id = problem_basic.id").
+	// 		Where("pc.category_id = (SELECT cb.id FROM category_basic cb WHERE cb.identity = ? )", categoryIdentity)
+	// }
+	return tx
+}
---
+++ service/user.go
@@ -0,0 +1,123 @@
+package service
+
+import (
+	"gin-gorm-oj/helper"
+	"gin-gorm-oj/models"
+	"gorm.io/gorm"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GetUserDetail
+// @Tags 公共方法
+// @Summary 用户详情
+// @Param identity query string false "identity"
+// @Success 200 {string} json "{"code":"200","data":""}"
+// @Router /user-detail [get]
+func GetUserDetail(c *gin.Context) {
+	identity := c.Query("identity")
+	if identity == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "用户唯一标识不能为空",
+		})
+		return
+	}
+	// 用户唯一标识不为空
+	data := new(models.UserBasic)
+	err := models.DB.Omit("password").Where("identity = ?", identity).Find(&data).Error
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "Get user detail by identity:" + identity + "Error" + err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": data,
+	})
+}
+
+// Login
+// @Tags 公共方法
+// @Summary 用户登录
+// @Param username formData string false "username"
+// @Param password formData string false "password"
+// @Success 200 {string} json "{"code":"200","data":""}"
+// @Router /login [post]
+func Login(c *gin.Context) {
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+	if username == "" || password == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"data": -1,
+			"msg":  "必填信息为空",
+		})
+		return
+	}
+	password = helper.GetMd5(password) // 对密码加密
+	println(username, password)
+
+	data := new(models.UserBasic)
+	err := models.DB.Where("name = ? AND password = ?", username, password).First(&data).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusOK, gin.H{
+				"data": -1,
+				"msg":  "用户名或密码错误",
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": -1,
+			"msg":  "Get UserBasic Error:" + err.Error(),
+		})
+		return
+	}
+	token, err := helper.GenerateToken(data.Identity, data.Name)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"data": -1,
+			"msg":  "GenerateToken Error:" + err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": -1,
+		"data": map[string]interface{}{
+			"token": token,
+		},
+	})
+}
+
+// SendCode
+// @Tags 公共方法
+// @Summary 发送验证码
+// @Param email formData string true "email"
+// @Success 200 {string} json "{"code":"200","data":""}"
+// @Router /send-code [post]
+func SendCode(c *gin.Context) {
+	email := c.PostForm("email")
+	if email == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "参数不正确",
+		})
+		return
+	}
+	code := "123456"
+	err := helper.SendCode(email, code)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "Send Code err:" + err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "验证码发送成功！",
+	})
+}
